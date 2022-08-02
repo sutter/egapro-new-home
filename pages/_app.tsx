9abd5561563fbae31fef7908c8661802e7ab9bae
Laurent Sutterlity <laurent@sutterlity.studio>
2022-08-02 17:15:57 +0200

    refactor components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,15 @@
 import "../styles/globals.css"
 
 import type { AppProps } from "next/app"
-import { Skiplinks as DsfrSkiplinks, SkiplinkItem } from "@dataesr/react-dsfr"
 
 import Header from "../components/Header"
 import Footer from "../components/Footer"
+import SkipLinks from "../components/SkipLinks"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <DsfrSkiplinks>
-        <SkiplinkItem href="#contenu">Contenu</SkiplinkItem>
-        <SkiplinkItem href="#header-navigation">Menu</SkiplinkItem>
-        <SkiplinkItem href="#footer">Pied de page</SkiplinkItem>
-      </DsfrSkiplinks>
+      <SkipLinks />
       <Header />
       <div id="contenu">
         <Component {...pageProps} />
