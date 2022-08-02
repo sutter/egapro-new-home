ead2163a36e1023dfc641b51dce7ea3ac39e487a
Laurent Sutterlity <laurent@sutterlity.studio>
2022-08-02 17:01:01 +0200

    fix: skiplink component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,19 @@
 import "../styles/globals.css"
 
 import type { AppProps } from "next/app"
+import { Skiplinks as DsfrSkiplinks, SkiplinkItem } from "@dataesr/react-dsfr"
 
-import SkipLinks from "../components/SkipLinks"
 import Header from "../components/Header"
 import Footer from "../components/Footer"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <SkipLinks />
+      <DsfrSkiplinks>
+        <SkiplinkItem href="#contenu">Contenu</SkiplinkItem>
+        <SkiplinkItem href="#header-navigation">Menu</SkiplinkItem>
+        <SkiplinkItem href="#footer">Pied de page</SkiplinkItem>
+      </DsfrSkiplinks>
       <Header />
       <div id="contenu">
         <Component {...pageProps} />
